e6b56dc56e7b5fff9d5266ce5e49a6218fdffc5d
VedangiPatel10 <vedangipatel1005@gmail.com>
2026-06-19 15:10:46 -0400

    Added error handler

--- ASSIGNMENT01/app.js
+++ ASSIGNMENT01/app.js
@@ -19,3 +19,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
+app.use('/', indexRouter);
+
+// 404 handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// Error handler
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Vedangi's Portfolio running on port ${PORT}`);
+});
+
+module.exports = app;
