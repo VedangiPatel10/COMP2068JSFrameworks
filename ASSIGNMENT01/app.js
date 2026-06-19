4914043702092c98a6901e37567ce039ca73574b
VedangiPatel10 <vedangipatel1005@gmail.com>
2026-06-19 15:02:47 -0400

    Added register for header and footer and middleware

--- ASSIGNMENT01/app.js
+++ ASSIGNMENT01/app.js
@@ -10,3 +10,12 @@ var app = express();
 // View engine setup 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+// Register partials like header and footer
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
+// Middleware to parse request body
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
