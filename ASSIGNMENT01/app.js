2525cfd56a1c77e9f8f2ef96c39d76dceca143c9
VedangiPatel10 <vedangipatel1005@gmail.com>
2026-06-19 14:58:08 -0400

    added view engine setup

--- ASSIGNMENT01/app.js
+++ ASSIGNMENT01/app.js
@@ -2,3 +2,11 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var hbs = require('hbs');
+
+var indexRouter = require('./routers/index');
+
+var app = express();
+
+// View engine setup 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
