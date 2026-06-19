2b0125b4a881860eb7ef043bd91f427bfc946ffd
VedangiPatel10 <vedangipatel1005@gmail.com>
2026-06-19 14:36:06 -0400

    Made home page and about me page details.

--- ASSIGNMENT01/routers/index.js
+++ ASSIGNMENT01/routers/index.js
@@ -1,3 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+// Home Page - giving the basic information about the website and the author
+router.get('/', function (req, res) {
+  res.render('index', {
+    title: 'Vedangi Patel | Home',
+    page: 'home'
+  });
+});
+
+// About Me Page - giving the information about the author
+router.get('/about', function (req, res) {
+  res.render('about', {
+    title: 'Vedangi Patel | About Me',
+    page: 'about'
+  });
+});
+
